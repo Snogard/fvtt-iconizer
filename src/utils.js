ab774e5947633dea485b89f25de00c407c700a89
Snogard <fulgiale@gmail.com>
2020-04-12 15:09:01 +0200

    formatted utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,9 @@
-let utils = 
-{
-    async serverFileExists(path)
-    {
-        return fetch(path, {method: "HEAD"})
-        .then(resp =>{
-            return resp.status < 400;
-        })
-        .catch(err=>false);
-    }
+let utils = {
+    async serverFileExists(path) {
+        return fetch(path, { method: "HEAD" })
+            .then(resp => {
+                return resp.status < 400;
+            })
+            .catch(err => false);
+    },
 }
